a029cd58004d9fa165c4d1ffd1fa97ced2ea8b59
raul-guerrero-c <mlr.guerrero.c@gmail.com>
2026-01-12 20:12:59 -0600

    Delete db/mysql directory

--- db/mysql/00_create_database.sql
+++
@@ -1,6 +0,0 @@
--- db/mysql/00_create_database.sql
--- Crea la base de datos principal de la tienda online.
-
-CREATE DATABASE IF NOT EXISTS online_store
-  CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
